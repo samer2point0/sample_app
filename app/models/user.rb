557ab6c55e165aabdd0141922b040ff2def2b54c
Samer Mohamed <Samirsuraj@live.com>
2018-01-23 15:04:36 +0200

    added name and email validations, from now testing is implicit and is done before each commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  ValidEmailRegEx= /\A[\w+\d\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, {presence: true, length: {maximum: 50}}
+  validates :email, {presence: true, length:{maximum: 255},
+                     format: {with: ValidEmailRegEx},
+                     uniqueness:{case_sensitive: false}
+                     }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user=User.new(name: "samer", email: "samir-suraj.94@live.com")
+  end
+  test "should be valid" do
+    assert @user.valid?
+  end
+  test "name should be pressent" do
+    @user.name=""
+    assert_not @user.valid?
+  end
+  test "email should be pressent" do
+    @user.email=""
+    assert_not @user.valid?
+  end
+  test "name should not be too long" do
+    @user.name="s"*51
+    assert_not @user.valid?
+  end
+  test "email should not be too long" do
+    @user.email="s"*250+"@live.com"
+    assert_not @user.valid?
+  end
+  test "email validation should reject invalid email address" do
+    emails=%w[samirsuraj@live,com samir.com samersuraj@live. samir@suraj+live.com samirsuraj@live..com]
+    emails.each{ |invemail|
+      @user.email=invemail
+      assert_not @user.valid?, "#{invemail.inspect} should be invalid"
+    }
+  end
+  test "email should be unique" do
+    dupuser=@user.dup
+    @user.save
+    dupuser.email=dupuser.email.upcase
+    assert_not dupuser.valid?
+  end
 end
