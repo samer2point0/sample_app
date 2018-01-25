663eec46cb585f7a5dde6134676496cc72322ac6
Samer Mohamed <Samirsuraj@live.com>
2018-01-25 20:24:24 +0200

    add user show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "/help", to: 'static_pages#help'
   get "/contact", to: "static_pages#contact"
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
