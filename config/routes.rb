9f2e4113c11b418c718a7e8688ed2e8acf847d4e
Samer Mohamed <Samirsuraj@live.com>
2018-01-30 23:21:29 +0200

    add logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact"
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
