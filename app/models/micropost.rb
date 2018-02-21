df14e04f598c77a3943b5958e7ca73ef25c215d2
Samer Mohamed <samirsuraj@live.com>
2018-02-21 22:16:14 +0200

    add image uploads

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,17 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :user_id, presence:true
   validates :content, presence: true, length:{maximum:140}
+  mount_uploader :picture, PictureUploader
   default_scope -> {order(created_at: :desc)}
+  validate :picture_size
+
+
+
+  private
+    def picture_size
+      #self can be ignorred inside the model
+      if picture.size>5.megabytes
+        errors.add(:picture, 'should be less than 5 megabytes')
+      end
+    end
 end
